0bc1defd7532b4f079ffe068228dcea8c0ca2f29
LeeshaSPIT <leeshaypatil@gmail.com>
2026-06-12 11:27:59 +0530

    fix: move firebase keys to env variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD4B7JteWT0wH9Dgy2nRyFitUMUnAFDggE",
-  authDomain: "community-connect-f9425.firebaseapp.com",
-  projectId: "community-connect-f9425",
-  storageBucket: "community-connect-f9425.firebasestorage.app",
-  messagingSenderId: "785076540540",
-  appId: "1:785076540540:web:2b84c8a00558ce1af025f9",
-  measurementId: "G-NGGQWGNCMY"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
